ff8a9793fb431e0e7c4579572ba56c0d4f80c066
Douglas Gonçalves <douglasgoncalves@MacBook-Air-de-Douglas.local>
2020-02-23 18:59:03 -0300

    Commiting...

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Welcome to InstaClone by DougGonçalves
+
+This is a work in progress and we love that you have joined us.
+
+
+All the best,<br>
+Douglas
+@endcomponent 
